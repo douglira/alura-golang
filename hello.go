8ae909ecb8cdf12cee4962fd677e16f822ff976e
Douglas Lira <douglas.vclira@gmail.com>
2022-04-28 12:07:13 -0300

    go hello world

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Função com letra maiúsculina - Convenção da linguagem para
+	informar que é uma função de um pacote/modulo externo
+	**/
+	fmt.Println("Olá Mundo, Douglas!")
+}
